d3619bf50a8bc69f3319e93f1fbbb1f9991fb020
Макс Титов <hhit934@gmail.com>
2022-10-07 13:20:43 +0300

    Добавьте файлы проекта.

---
+++ WebClient/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using CefSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WebClient
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string[] args_ = Environment.GetCommandLineArgs();
+            if(args_.Length > 1)
+            {
+                browser.Address = args_[1];
+                browser.LoadingStateChanged += (o, e) =>
+                {
+                    if (e.IsLoading == false)
+                        browser.GetBrowser().MainFrame.GetSourceAsync().ContinueWith(taskHtml =>
+                        {
+                            var html = taskHtml.Result;
+                            File.WriteAllText("index.html", html);
+
+                        });
+
+
+                };
+                if (args_[3] != string.Empty && Directory.Exists(args_[3]))
+                {
+                    browser.ExecuteScriptAsync(File.ReadAllText(args_[3]));
+
+                }
+                
+            }
+            
+        } 
+    }  
+}
---
+++ WebClientRuntime/Program.cs
@@ -0,0 +1,57 @@
+﻿using CefSharp.Wpf;
+using System;
+using System.IO;
+using System.Security.Policy;
+
+namespace WebClientRuntime
+{
+
+    internal class Program
+    {
+        static ChromiumWebBrowser browser;
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+
+            browser = new ChromiumWebBrowser();
+            _d_event(browser);
+            if (args.Length == 0)
+            {
+
+               
+                while (true)
+                {
+                    browser.Address = Console.ReadLine();
+                    
+                    Console.WriteLine("ViewSource");
+                }
+            }
+            else
+            {
+                browser.Address = args[0];
+            }
+
+
+            void _d_event( ChromiumWebBrowser browser)
+            {
+                
+
+                browser.FrameLoadEnd += (o, e) =>
+                {
+                    if (e.Frame.IsMain)
+                    {
+                        Console.WriteLine("LoadEnd");
+                        browser.GetBrowser().MainFrame.GetSourceAsync().ContinueWith(taskHtml =>
+                        {
+                            var html = taskHtml.Result;
+                            File.WriteAllText("index.html", html);
+                           
+                        });
+                    }
+                };
+            }
+        }
+        
+    }
+}
